9c2d7fb1de1becb3449eb7313a39292a08ad025e
Andrey <andrey.mikhaylov.n@gmail.com>
2024-10-18 13:20:21 +0300

    done

--- module_9_2.py
+++ module_9_2.py
@@ -1,3 +1,35 @@
+
+def test():
+    first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+    second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+    # В переменную first_result запишите список созданный при помощи сборки состоящий из длин строк списка first_strings,
+    # при условии, что длина строк не менее 5 символов.
+    first_result = [len(word) for word in first_strings if len(word) >= 5]
+
+    # В переменную second_result запишите список созданный при помощи сборки состоящий из пар слов(кортежей) одинаковой длины.
+    # Каждое слово из списка first_strings должно сравниваться с каждым из second_strings. (два цикла)
+    second_result = [(word1, word2) for word1 in first_strings for word2 in second_strings if len(word1) == len(word2)]
+
+    # В переменную third_result запишите словарь созданный при помощи сборки, где парой ключ-значение будет строка-длина строки.
+    # Значения строк будут перебираться из объединённых вместе списков first_strings и second_strings. Условие записи пары в словарь - чётная длина строки.
+    third_result = [{word: len(word)} for word in first_strings + second_strings if len(word) % 2 == 0]
+
+    print(first_result)
+    print(second_result)
+    print(third_result)
+    """
+    Вывод на консоль:
+    [10, 8, 8]
+    [('Elon', 'Task'), ('Elon', 'Java'), ('Musk', 'Task'), ('Musk', 'Java'), ('Monitors', 'Computer'), ('Variable', 'Computer')]
+    {'Elon': 4, 'Musk': 4, 'Programmer': 10, 'Monitors': 8, 'Variable': 8, 'Task': 4, 'Java': 4, 'Computer': 8}
+    """
+
+
+if __name__ == '__main__':
+    test()
+
+
 """
 2023/11/30 00:00|Домашнее задание по теме "Списковые, словарные сборки"
 Если вы решали старую версию задачи, проверка будет производиться по ней.
